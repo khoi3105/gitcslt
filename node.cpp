14b4c7dfcc4bebac317286f6023883b2941f2c72
Dang Khoi Ngo <ngodangkhoi580@gmail.com>
2025-12-09 09:49:10 +0700

    add function for node

--- node.cpp
+++ node.cpp
@@ -1,15 +1,74 @@
 #include <iostream>
 using namespace std;
 
-struct List {
-};
-
 struct Node
 {
+    int value;
+    Node *pNext;
 };
 
+struct List {
+    Node *pHead;
+};
 
+Node* createNode(int value) {
+    Node* newNode = new Node;
+    newNode->value = value;
+    newNode->pNext = NULL;
+    return newNode;
+}
+
+bool isEmpty(List list) {
+    return (list.pHead == NULL);
+}
+
+void addHead(List& list, Node* node) {
+    if (list.pHead == NULL) {
+        list.pHead = node;
+    } else {
+        node->pNext = list.pHead;
+        list.pHead = node;
+    }
+}
+
+void addTail(List& list, Node* node) {
+    if (list.pHead == NULL) {
+        list.pHead = node;
+    } else {
+        Node* temp = list.pHead;
+        while (temp->pNext != NULL) {
+            temp = temp->pNext;
+        }
+        temp->pNext = node;
+    }
+}
+
+void displayList(List list) {
+    Node* temp = list.pHead;
+    while (temp!=NULL) {
+        cout << temp->value << " ";
+        temp=temp->pNext;
+    }
+    cout << endl;
+}
+
+Node* findByValue(List list, int value) {
+    
+}
 
 int main() {
+    List list;
+    list.pHead = NULL;
+    Node* node1 = createNode(1);
+    Node* node2 = createNode(2);
+    Node* node3 = createNode(3);
+    Node* node4 = createNode(4);
+
+    addHead(list,node1);
+    addTail(list,node2);
+    addTail(list,node3);
+    addTail(list,node4);
+
+    displayList(list);
     return 0;
 }
